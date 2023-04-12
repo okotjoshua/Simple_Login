d737ca8a9ed17292efd7eedf9c06c5856e1d87f8
okotjoshua <okotjoshua43@gmail.com>
2023-04-12 08:01:24 +0300

    Updated clock

--- script.js
+++ script.js
@@ -16,4 +16,33 @@ function validateForm() {
       return false;
     }
   }
+  function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    
+    setTimeout(updateClock, 1000);
+  }
+  
+  updateClock();
+  
   
